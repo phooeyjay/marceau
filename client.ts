a3e114c31b85b9f58ce401ab851e03e660295c1d
Wetcher <13554906+phooeyjay@users.noreply.github.com>
2023-11-27 10:43:43 +0800

    Upgrade version, dated 20231127

--- client.ts
+++ client.ts
@@ -1,57 +1,50 @@
-import { Client } from 'discord.js';
-import * as Commands from './commands';
+import { Client, Collection, REST, Routes, inlineCode } from 'discord.js';
+import { Logger, throwexc } from './utils';
+import { ERR_RETRY } from './constants';
 
 export class ExtendedClient extends Client {
-    static create () {
+    private dictionary = new Collection<string, any>();
+
+    logout = () => (async () => { await this.destroy(); process.exit(1); })();
+    static create = () => {
         const client = new ExtendedClient({
             intents: [ 'Guilds', 'GuildMembers', 'GuildMessages', 'GuildMessageReactions' ]
             , presence: { status: process.env.DEBUG && 'invisible' || 'online' }
         });
 
         //#region Initializing event listeners
+        client.once('ready', async c => {
+            try {
+                (await import('node:fs')).readdirSync(`./slashcommands`).filter(f => f.endsWith('.ts')).forEach(async f => (imp => {
+                    ('data' in imp && 'execute' in imp) && client.dictionary.set(imp.data.name, imp) || throwexc(`${f}: missing _data_ or _execute_.`);
+                })(await import(`./slashcommands/${f}`)));
+
+                await (new REST().setToken(process.env.TOKEN!)).put(Routes.applicationCommands(process.env.APPID!), { 
+                    body: Array.from(client.dictionary.values()).map(v => v.data.toJSON()) 
+                });
+                Logger.cmdl(`LOGIN: ${c.user.username}`);
+            } catch (ex) { Logger.cmdl(ex); client.logout(); }
+        });
         client.on('interactionCreate', async i => {
-            throw 'Not Implemented';
-        })
+            if (i.isChatInputCommand()) {
+                try {
+                    const cmd = client.dictionary.get(i.commandName) || throwexc(`${i.commandName}: Get failed.`);
+                    await cmd.execute(i);
+                    Logger.command(i);
+                } catch (ex) {
+                    if (i.replied || i.deferred) {
+                        await i.deleteReply();
+                        await i.channel?.send(ERR_RETRY);
+                    }
+                    else await i.reply({ content: ERR_RETRY });
+                    Logger.command(i, ex);
+                }
+            }
+        });
+        client.on('guildMemberRemove', async member => Logger.write(`${inlineCode(member.user.username || member.user.tag)} has left the server.`));
+        ['SIGINT', 'SIGTERM'].forEach(sig => process.on(sig, () => { Logger.cmdl('Exiting.'); client.logout(); }));
         //#endregion
+
+        return client;
     }
 }
-
-// import { Client, Collection, REST, Routes, inlineCode } from 'discord.js';
-// import { Logger, Scheduler, throwexc } from './utils';
-// import { ERR_RETRY } from './constants';
-// import { dataset, route } from './routecommands'
-
-// export class ExtendedClient extends Client {
-//     logout = () => (async () => { await this.destroy(); Scheduler.halt(); process.exit(1); })()
-
-//     static initialize = () => {
-//         const client = new ExtendedClient({ 
-//             intents:    [ 'Guilds', 'GuildMembers', 'GuildMessages', 'GuildMessageReactions' ],
-//             presence:   { status: process.env.DEBUG && 'invisible' || 'online' } // Whatever the fuck 'invisible' is in this context.
-//         });
-
-//         //#region Event listeners - initialization
-//         client.once('ready', async c => {
-//             try {
-//                 await (new REST().setToken(process.env.TOKEN!)).put(Routes.applicationCommands(process.env.APPID!), { body: dataset });
-//                 Logger.cli(`LOGIN: ${c.user.username}`);
-//             } catch (err) { Logger.cli('[Client.ready] error:', err); client.logout();  }
-//         });
-//         client.on('interactionCreate', async i => {
-//             if (i.isChatInputCommand()) {
-//                 try {
-//                     await route(i.commandName, i);
-//                     Logger.command(i);
-//                 } catch (err) {
-//                     (i.replied || i.deferred) && await i.deleteReply().then(async () => await i.channel?.send(ERR_RETRY)) || await i.reply({ content: ERR_RETRY });
-//                     Logger.command(i, err);
-//                 }
-//             }
-//         });
-//         client.on('guildMemberRemove', async member => Logger.plaintext(`${inlineCode(member.user.username || member.user.tag)} has left the server.`));
-//         ['SIGINT', 'SIGTERM'].forEach(sig => process.on(sig, () => { Logger.cli('Exiting.'); client.logout() }));
-//         //#endregion
-
-//         return client;
-//     }
-// }
